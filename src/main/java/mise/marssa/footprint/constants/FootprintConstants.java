a9923eb37b3e7e5a398232a0dfab03d0894276a0
Clayton Tabone <taboneclayton@gmail.com>
2011-12-16 20:30:46 +0100

    Renamed Constants.java to FootprintConstants.java

--- src/main/java/mise/marssa/footprint/constants/FootprintConstants.java
+++ src/main/java/mise/marssa/footprint/constants/FootprintConstants.java
@@ -1,7 +1,7 @@
 /**
  * 
  */
-package mise.marssa.footprint;
+package mise.marssa.footprint.constants;
 
 import mise.marssa.footprint.data_types.integer_datatypes.MInteger;
 
@@ -9,7 +9,7 @@ import mise.marssa.footprint.data_types.integer_datatypes.MInteger;
  * @author Clayton Tabone
  *
  */
-public final class Constants {
+public final class FootprintConstants {
 	public final static MInteger RETRY_AMOUNT = new MInteger(5);
 	public final static MInteger RETRY_INTERVAL = new MInteger(5);
 	public final static MInteger VALUE_OUTOFRANGE = new MInteger(0);
