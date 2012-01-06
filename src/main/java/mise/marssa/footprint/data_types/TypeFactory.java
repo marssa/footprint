409a0240ab26c6cc7c728e183c6738f37ea78010
alangrech <alan.grech@marsec-xl.org>
2012-01-06 09:40:40 +0100

    Added Frequency methods to TypeFactory.java

--- src/main/java/mise/marssa/footprint/data_types/TypeFactory.java
+++ src/main/java/mise/marssa/footprint/data_types/TypeFactory.java
@@ -3,11 +3,15 @@
  */
 package mise.marssa.footprint.data_types;
 
+import mise.marssa.footprint.data_types.float_datatypes.frequency.AFrequency;
+import mise.marssa.footprint.data_types.float_datatypes.frequency.Hz;
+import mise.marssa.footprint.data_types.float_datatypes.frequency.KHz;
 import mise.marssa.footprint.data_types.time.ATime;
 import mise.marssa.footprint.data_types.time.Hours;
 import mise.marssa.footprint.data_types.time.Milliseconds;
 import mise.marssa.footprint.data_types.time.Minutes;
 import mise.marssa.footprint.data_types.time.Seconds;
+import mise.marssa.footprint.exceptions.OutOfRange;
 
 
 
@@ -35,5 +39,16 @@ public class TypeFactory {
 	
 	public static ATime getATimeInstance() {
 		return new Milliseconds(100l);
+	}
+	
+	//Frequenecy
+	public static AFrequency getAFrequencyInstance() throws OutOfRange {
+		return new Hz(100l);
+	}
+	public static Hz getHzInstance() throws OutOfRange {
+		return new Hz(100l);
+	}
+	public static KHz getKHzInstance() throws OutOfRange {
+		return new KHz(100l);
 	}
 }
